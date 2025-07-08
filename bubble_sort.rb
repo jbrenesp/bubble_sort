3f24f00b6bb066da391a207e771885324fa4efda
jbrenesp <josembrenes@gmail.com>
2025-07-08 14:13:45 +0300

    bubble sort ruby code finished

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,5 +1,12 @@
 def bubble_sort(array)
-    array.each_with_index do |value, index|
-
+    n = array.length
+    (n - 1).times do
+        array.each_with_index do |value, index|
+            break if index == n - 1
+            if array[index] > array[index + 1]
+                array[index], array[index + 1] = array[index + 1], array[index]
+             end
+        end
     end
+    array
 end
